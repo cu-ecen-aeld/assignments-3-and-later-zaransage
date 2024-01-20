42c8a083561174a6e4aae24f4a05be18bfea1df5
Dana Marble <dana.marble@gmail.com>
2024-01-20 03:47:41 +0000

    Initial Commit of working prototype

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Dana Marble
+# Assignment 1
+
 filesdir=$1
 searchstr=$2
 
+#########################################################
+# Check to see if the first argument is specified
+#########################################################
 
 if [ -z ${filesdir} ]
 then
@@ -10,11 +16,19 @@ echo "You have not specified 'filesdir' argument"
 exit 1
 fi
 
+#########################################################
+# Check to see if the directory to find exists
+#########################################################
+
 if [ ! -d ${filesdir} ]
 then
 echo "This directory does not exist"
 exit 1
 fi
+
+#########################################################
+# Check to see if the search is specified
+#########################################################
 
 if [ -z ${searchstr} ]
 then
@@ -22,3 +36,54 @@ echo "You have not specified 'searchstr' argument"
 exit 1
 fi
 
+#########################################################
+# Build array of files with a count of the search string only
+#########################################################
+
+list_build(){
+
+LineArray=($( grep -risc ${searchstr} ${filesdir} | grep -v ':0'))
+
+}
+
+#########################################################
+# Using the data in the array, get the line count
+#########################################################
+
+list_get_lines(){
+
+LINES=$(for i in ${LineArray[@]}; do echo $i;done| wc -l)
+echo $LINES
+
+}
+
+#########################################################
+# Capture only the quantity of the search tearm and sum them
+# Probably, there is a better way to do this.
+#########################################################
+
+list_get_quantity(){
+TOTAL=$(for i in ${LineArray[@]}; do echo $i | cut -f2 -d ":";done)
+COUNT=0; for i in $TOTAL; do COUNT=$(expr $i + $COUNT) ;done
+
+echo $COUNT
+
+}
+
+#########################################################
+# Call the list and format the output.
+# Arguably I should use a seperate method for this part too.
+#########################################################
+
+main(){
+    
+    list_build
+    echo "The number of files are $(list_get_lines) and the number of matching lines are $(list_get_quantity)"    
+
+}
+
+#########################################################
+# Actually call the main function
+#########################################################
+
+main
