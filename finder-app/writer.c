4767849eb23895520797a798fa68abe757c19816
Dana Marble <dana.marble@gmail.com>
2024-01-28 20:33:48 +0000

    Make file, output from commands and clean up binaries

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -11,22 +11,6 @@ Outside Reference: https://www.tutorialspoint.com/cprogramming/c_command_line_ar
 */
 
 
-/* 
-One difference from the write.sh instructions in Assignment 1:  
-You do not need to make your "writer" utility create directories which do not exist.  
-You can assume the directory is created by the caller.
-
-Setup syslog logging for your utility using the LOG_USER facility.
-
-Use the syslog capability to write a message “Writing <string> to <file>” where <string> 
-is the text string written to file (second argument) and <file> is the file created by the script.  
-This should be written with LOG_DEBUG level.
-
-Use the syslog capability to log any unexpected errors with LOG_ERR level. 
- 
- */
-
-
 int file_create(char *file, char *message){
   const int err = errno;
 
