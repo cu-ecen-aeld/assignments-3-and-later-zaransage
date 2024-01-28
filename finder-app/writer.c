1b44f36c3c69e626e1dc2458e1fa38648ac09744
Dana Marble <dana.marble@gmail.com>
2024-01-28 19:25:38 +0000

    Improve logging and errors

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -28,6 +28,7 @@ Use the syslog capability to log any unexpected errors with LOG_ERR level.
 
 
 int file_create(char *file, char *message){
+  const int err = errno;
 
   int fd;
   const char *buf;
@@ -36,14 +37,14 @@ int file_create(char *file, char *message){
   fd = open(file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
   if (fd == -1){
-    printf("File does not exist: %d", fd);
-    syslog(LOG_ERR, "File does not exist:\n %d", fd);
+    printf("File does not exist\n: %d", fd);
+    syslog(LOG_ERR, "File does not exist: %d: %s\n", fd, strerror(err));
     exit(1);
   } 
 
     buf = message;
     nr = write(fd, buf, strlen(buf));
-    syslog(LOG_DEBUG, "Writing %s to %s:\n", message, file);
+    syslog(LOG_DEBUG, "Writing %s to %s: %s", message, file, strerror(err));
 }
 
 
@@ -53,21 +54,21 @@ int main(int argc, char *argv[]){
   openlog(NULL,0,LOG_USER);
 
   if(argc == 1) {
-    printf("You need two arguments but you only gave: None\n");
-    syslog(LOG_ERR,"Invalid number of arguments: No arguments given");
+    printf("You have not specified 'writefile' argument\n");
+    syslog(LOG_ERR,"Invalid number of arguments: You have not specified a \'writefile\' argument: %s", strerror(err));
     exit(1);
   }
   if(argc < 3) {
-    printf("You need two arguments but you only gave: %s\n", argv[1]);
-    syslog(LOG_ERR,"Invalid number of arguments: %s", argv[1]);
+    printf("You have not specified writestr argument\n");
+    syslog(LOG_ERR,"Invalid number of arguments: You have not specified a \'writestr\' argument: %s", strerror(err));
     exit(1);
   }
   if(argc > 3) {
-    printf("You need only two arguments but you gave: %s %s %s\n", argv[1], argv[2], argv[3]);
-    syslog(LOG_ERR, "You need only two arguments but you gave: %s %s %s\n", argv[1], argv[2], argv[3]);
+    printf("You need only two arguments but you gave: %s %s %s %s\n", argv[1], argv[2], argv[3], strerror(err));
+    syslog(LOG_ERR, "You need only two arguments but you gave: %s %s %s %s\n", argv[1], argv[2], argv[3], strerror(err));
     exit(1);
   }
 
-    file_create(argv[1], argv[2]);
+  file_create(argv[1], argv[2]);
 
 }
