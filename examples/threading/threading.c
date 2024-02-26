d164512b1fd4486f3484023c43ef961746e9850d
Dana Marble <dana.marble@gmail.com>
2024-02-26 01:20:38 +0000

    better but I think not dynamic

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,20 +2,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
-// You haven't earned the right to retire. You have no permission to give up.
+
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
-
-#define handle_error_en(en, msg) \
-        do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
-#define handle_error(msg) \
-        do { perror(msg); exit(EXIT_FAILURE); } while (0)
-
-
 
 void* threadfunc(void* thread_param)
 {
@@ -26,11 +18,11 @@ void* threadfunc(void* thread_param)
 
     struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
-    sleep(thread_func_args->wait_to_obtain_ms);
+    usleep(thread_func_args->wait_to_obtain_ms);
 
     pthread_mutex_lock(&thread_func_args->mutex);
 
-    sleep(thread_func_args->wait_to_release_ms);
+    usleep(thread_func_args->wait_to_release_ms);
 
     pthread_mutex_unlock(&thread_func_args->mutex);
 
@@ -57,17 +49,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int
         *mutex,
         wait_to_obtain_ms,
         wait_to_release_ms,
+        true,
     };
     
-    pthread_mutex_init(&my_thread_data.mutex, NULL);
-    
-    s = pthread_create(&my_thread_data.thread, NULL, threadfunc, NULL);
+    s = pthread_mutex_init(&my_thread_data.mutex, NULL);
         if (s != 0)
-            handle_error_en(s, "pthread_create");
+            return false;
 
-    s = pthread_join(my_thread_data.thread, NULL);
+    s = pthread_create(&my_thread_data.thread, NULL, threadfunc, &my_thread_data);
+        if (s != 0)
+            return false;
+    
+    pthread_join(my_thread_data.thread, NULL);
 
-    s = pthread_mutex_destroy(&my_thread_data.mutex);
+    pthread_mutex_destroy(&my_thread_data.mutex);
 
 
     return false;
