3c514122ec9bf95916b4575fe5a3c25cb3ef2c85
Dana Marble <dana.marble@gmail.com>
2024-03-07 22:48:01 +0000

    I think its at least not angry at me

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -16,116 +16,98 @@
 #define FILEPATH "/var/tmp/aesdsocketdata"
 
 
+int main(int argc, char *argv[]){
+    int sockfd, new_fd;
+    struct addrinfo hints, *serverinfo, *p;
+    struct sockaddr_storage connect_addr;
+    socklen_t sin_size;
+    char s[INET6_ADDRSTRLEN];
+    int yes = 1;
+    int rv;
 
+    openlog(NULL, 0, LOG_USER);
 
-int main( int argc, char *argv[]){
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-  int sockfd, new_fd;
-  struct addrinfo hints, *serverinfo, *p;
-  struct SOCKADDR *ai_addr;
-  struct sockaddr_storage connect_addr;
-  socklen_t sin_size;
-  struct sigaction sig_action;
-  int yes = 1;
-  int rv;
-  char s[INET6_ADDRSTRLEN];
-
-  FILE *fp = fopen(FILEPATH, "w");
-
-  openlog(NULL,0,LOG_USER);
-
-  memset(&hints, 0, sizeof hints);
-
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &serverinfo)) != 0){
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-    return 1;
-  }
-
-  // Everything I read says I am missing a loop here.
-
-for (p = serverinfo; p != NULL; p = p->ai_next){
-
-  if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1){
-    perror("Unable to open socket");
-    syslog(LOG_ERR,"Unable to open socket");
-     continue;
-  }
-
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
-    perror("Unable to open socket: opt error");
-    syslog(LOG_ERR,"Socket opt error");
-    return 1;
-  }
-
-  if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-    close(sockfd);
-    perror("Unable to bind socket");
-    syslog(LOG_ERR,"Unable to bind socket");
-    continue;
-  };
-    break;
-  }
-
-  freeaddrinfo(serverinfo);
-
-  /*Heavily borrowed from beej.us*/
-
-  if (p == NULL){
-    fprintf(stderr, "failed to bind");
-    perror("Unable to bind socket");
-    syslog(LOG_ERR,"Unable to bind socket");
-    return 1;
-
-  }
-
-  if (listen(sockfd, 10) == -1){
-    fprintf(stderr, "failed to bind");
-    perror("Unable to bind socket");
-    syslog(LOG_ERR,"Unable to bind socket");
-    return 1;
-  }
-
-  while(1){
-    sin_size = sizeof connect_addr;
-    new_fd = accept(sockfd, (struct sockaddr *) &connect_addr, &sin_size);
-
-    if (new_fd < 0){
-        perror("Unable to accept socket");
-        syslog(LOG_ERR,"Unable to accept socket");
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &serverinfo)) != 0){
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
-        continue;
     }
 
-    if (connect_addr.ss_family == AF_INET){
-        struct sockaddr_in *ipv4 = (struct sockaddr_in *) &connect_addr;
-        inet_ntop(AF_INET, &(ipv4->sin_addr), s, sizeof s);
-
-        printf("Accepted connection from %s\n",s);
-        syslog(LOG_INFO,"Accepted connection from %s\n",s);
-    }
-
-    if (!fork()){
-        close(sockfd);
-        if (send(new_fd, "connection accepted", 13, 0) == -1){
-            perror("failed to send");
+    for (p = serverinfo; p != NULL; p = p->ai_next){
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1) {
+            perror("Unable to open socket");
+            syslog(LOG_ERR, "Unable to open socket");
+            continue;
         }
-        close(new_fd);
-        perror("Unable to send data to socket");
-        syslog(LOG_ERR,"Unable to send data to socket");
-        return 0;
+
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1){
+            perror("Unable to set socket options");
+            syslog(LOG_ERR, "Socket option error");
+            return 1;
+        }
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+            close(sockfd);
+            perror("Unable to bind socket");
+            syslog(LOG_ERR, "Unable to bind socket");
+            continue;
+        }
+        break; 
     }
 
-    close(sockfd);
-    syslog(LOG_INFO, "Closed connection from %s\n" ,s);
+    freeaddrinfo(serverinfo);
 
-  }
+/*Borrowed the lower two if conditions heavily from beej.us*/
 
+    if (p == NULL){
+        fprintf(stderr, "server: failed to bind\n");
+        return 1;
+    }
+
+    if (listen(sockfd, 10) == -1){
+        perror("listen");
+        return 1;
+    }
+
+    while (1) {
+        sin_size = sizeof connect_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&connect_addr, &sin_size);
+
+        if (new_fd == -1){
+            perror("accept");
+            continue;
+        }
+
+        if (connect_addr.ss_family == AF_INET){
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)&connect_addr;
+            inet_ntop(AF_INET, &ipv4->sin_addr, s, sizeof s);
+        }
+
+        printf("Accepted connection from %s\n", s);
+        syslog(LOG_INFO, "Accepted connection from %s", s);
+
+        if (!fork()){ 
+            close(sockfd); 
+
+            if (send(new_fd, "connection accepted", strlen("connection accepted") + 1, 0) == -1){
+                perror("send");
+            }
+
+            close(new_fd);
+            exit(0);
+        }
+
+        close(new_fd);
+    }
+
+   
     close(sockfd);
-    remove(FILEPATH);
+    //remove(FILEPATH);
     closelog();
     return 0;
 }
