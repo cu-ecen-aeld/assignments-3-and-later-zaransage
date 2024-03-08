fe528c44c7fb84a1cf94fe1cbb0eb55c7c252933
Dana Marble <dana.marble@gmail.com>
2024-03-08 03:52:58 +0000

    Some kind of format error

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,14 +4,19 @@ PID="/var/run/aesdsocket.pid"
 
 case "$1" in
   start)
-  echo "Starting aesdsocket server"
-  /usr/bin/aesdsocket -d && echo $! > "${PID}"
+    echo "Starting aesdsocket server"
+    /usr/bin/aesdsocket -d
   ;;
   stop)
-  echo "Stopping aesdsocket server"
-  if [ -f "${PID}" ]; then kill $(cat ${PID}); rm -f "${PID}"; fi
+    echo "Stopping aesdsocket server"
+    if [ -f "${PID}" ]; then
+      kill $(cat ${PID})
+      rm -f "${PID}"
+    else
+      echo "aesdsocket server not running"
+    fi
   ;;
   *)
-  echo "Call either start or stop"
-  exit 1
+    echo "start|stop"
+    exit 1
 esac
