3a7410c6610bfacf4a4c7eb7d6237ec4cb0fb71c
Dana Marble <dana.marble@gmail.com>
2024-09-02 20:57:51 +0000

    Test functional version for later

---
+++ prototypes/time/time.prototype.0.0.2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+
+// https://datatracker.ietf.org/doc/html/rfc2822
+
+
+static void myTime() {
+
+    time_t now;
+    time(&now);
+
+    struct tm *local = localtime(&now);
+
+    char buffer[80];
+    // Mon 02 Sep 2024 08:53:12 +0000
+    strftime(buffer, sizeof(buffer), "%a %d %b %y %r %z", local);
+
+    printf("%s\n", buffer);
+
+}
+
+
+int main() {
+
+    myTime();
+    
+    return 0;
+}
