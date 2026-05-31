b75de73f89e402b8304c8d67b4bf308a6e841b66
Dana Marble <dana.marble@gmail.com>
2026-05-31 20:29:26 +0000

    Run some prototypes

---
+++ prototypes/queue-seek/prototype.0.0.1.c
@@ -0,0 +1,23 @@
+  #include <stdio.h>
+  #include "../../aesd-char-driver/aesd-circular-buffer.c"
+
+  int main() {
+
+      struct aesd_circular_buffer buffer;
+
+      aesd_circular_buffer_init(&buffer);
+
+      struct aesd_buffer_entry e0 = { "Grass\n", 6 };
+
+      struct aesd_buffer_entry e1 = { "Sentosa\n", 6 };
+
+      struct aesd_buffer_entry e2 = { "Singapore\n",  5 };
+
+      aesd_circular_buffer_add_entry(&buffer, &e0);
+      aesd_circular_buffer_add_entry(&buffer, &e1);
+      aesd_circular_buffer_add_entry(&buffer, &e2);
+
+      
+
+      return 0;
+  }
